e1be8a933c97b58ec0c43b2f7488eacddc64fefe
humblesami <Prvmgo123@45>
2021-02-12 03:23:04 +0500

    detailed subscription
    admin will add clients
    admin will add link client to one or more packages and define rates, connection charges
    
    admin will add client payment, for a client package, in result first user balance would be updated and the a subscription will be created with start and end date according to payment, start date will automatically be extended to one day after the end of user's current subscription for the same package if there is any available
    
    Testing needed, also some customization of admin side may be required

--- noman/admin.py
+++ noman/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Area, Package, PackageType, Client, Payment
+from .models import Area, Package, PackageType, Client, ClientPackage, Subscription, Payment
 
 
 class AreaAdmin(admin.ModelAdmin):
@@ -14,15 +14,15 @@ class PackageTypeAdmin(admin.ModelAdmin):
 
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display= ['name','status','package_type']
+    list_display= ['name','package_type']
     search_fields = ['name', 'package_type']
-    list_filter = ['package_type', 'status']
+    list_filter = ['package_type']
     exclude = ['created_at']
 
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ['name','mobile','email','next_payment_date','price','status']
-    search_fields = ['package']
+    list_display = ['area', 'name','mobile','email']
+    search_fields = ['package', 'area']
     exclude = ['created_at']
 
 
@@ -30,8 +30,14 @@ class PaymentAdmin(admin.ModelAdmin):
     exclude = ['created_at']
 
 
+class SubscriptionAdmin(admin.ModelAdmin):
+    exclude = ['end_date']
+
+
 admin.site.register(Area, AreaAdmin)
 admin.site.register(PackageType,PackageTypeAdmin)
 admin.site.register(Package, PackageAdmin)
-admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Client, ClientAdmin)
+admin.site.register(ClientPackage)
+admin.site.register(Payment, PaymentAdmin)
+admin.site.register(Subscription, SubscriptionAdmin)
--- noman/models.py
+++ noman/models.py
@@ -1,10 +1,8 @@
-import calendar
-
 from django.db import models
 from datetime import datetime
+from .import methods
 
 
-# Create your models here.
 class Area(models.Model):
     class Meta:
         ordering  = ('name', 'created_at')
@@ -38,6 +36,8 @@ class Package(models.Model):
     status = models.BooleanField()
     package_type = models.ForeignKey(PackageType, on_delete=models.CASCADE, related_name='packages', null=True)
     created_at = models.DateTimeField()
+    rate = models.IntegerField()
+    valid_for_months = models.IntegerField(default=1)
 
     def __str__(self):
         return self.name
@@ -51,15 +51,7 @@ class Package(models.Model):
 
 class Client(models.Model):
     name= models.CharField(max_length=200)
-    status = models.BooleanField()
-
-    price = models.IntegerField()
-    connection_charges= models.IntegerField()
-    connection_date = models.DateField()
-    pending = models.IntegerField(default=0)
-    package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='clients')
-    last_payment_date = models.DateField(blank=True, null=True)
-    next_payment_date = models.DateField()
+    area = models.ForeignKey(Area, related_name='clients', on_delete=models.CASCADE)
 
     father_name= models.CharField(max_length=200, null=True, blank=True)
     mobile = models.CharField(max_length=200, null=True, blank=True)
@@ -70,12 +62,15 @@ class Client(models.Model):
     created_at = models.DateTimeField()
 
     class Meta:
-        ordering = ('next_payment_date',)
+        ordering = ('area', 'name')
         pass
         # verbose_name_plural = 'Sami'
 
     def __str__(self):
         return self.name
+
+    def is_defaulter(self):
+        pass
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -85,16 +80,16 @@ class Client(models.Model):
         return res
 
 
-def add_months(sourcedate, months):
-    month = sourcedate.month - 1 + months
-    year = sourcedate.year + month // 12
-    month = month % 12 + 1
-    day = min(sourcedate.day, calendar.monthrange(year,month)[1])
-    return datetime.date(year, month, day)
+class ClientPackage(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    package = models.ForeignKey(Package, on_delete=models.CASCADE)
+    price = models.IntegerField()
+    connection_charges = models.IntegerField()
+    connection_date = models.DateField()
+    active = models.BooleanField(default=False)
 
-
-def add_one_month_to_date(given_date):
-    return add_months(given_date, 1)
+    def __str__(self):
+        return self.client.name + '-' + self.package.name
 
 
 class Payment(models.Model):
@@ -102,15 +97,75 @@ class Payment(models.Model):
         pass
         #verbose_name_plural = 'Sami'
 
-    client= models.ForeignKey(Client, related_name='payment', on_delete=models.CASCADE)
+    client_package = models.ForeignKey(ClientPackage, on_delete=models.CASCADE)
     amount = models.IntegerField()
     payment_date = models.DateField()
     created_at = models.DateTimeField()
 
     def save(self, *args, **kwargs):
+        creating = False
         if not self.pk:
+            creating = True
             self.created_at = datetime.now()
-            self.client.pending = self.client.pending - self.amount            
-            self.client.save()
         res = super().save(args, kwargs)
+
+        if creating:
+            self.client_package.client.balance + self.amount
+            self.client_package.client.save()
+
+            new_subscription = Subscription(
+                client_pakage_id=self.client_package.id,
+                start_date=self.payment_date,
+                payment_id=self.id
+            )
+            new_subscription.save()
+
         return res
+
+
+class Subscription(models.Model):
+    client_package = models.ForeignKey(ClientPackage, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+    def save(self, force_insert=False, force_update=False, using=None,
+             update_fields=None):
+        price = self.client_package.price
+        # in case of first subscription or subscription after service terminated
+        if not self.client_package.active:
+            price += self.client_package.connection_charges
+        if self.client_package.active:
+            obj = Subscription.objects.filter(client_package_id=self.client_package.id).order_by('end_date')
+            if len(obj):
+                last_obj = obj[len(obj) - 1]
+                if last_obj.end_date > self.start_date:
+                    diff_days = get_days_difference(last_obj.start_date, last_obj.end_date)
+                    self.start_date = add_days(last_obj.start_date, diff_days)
+
+        months = price % self.client.balance
+        end_date = add_months(self.start_date, months)
+        self.end_date=end_date
+        res = super().save()
+        return res
+
+
+def add_months(source_date, months):
+    res = methods.add_interval('months', months, source_date)
+    return res
+
+
+def add_one_month_to_date(given_date):
+    return add_months(given_date, 1)
+
+
+def get_days_difference(start_date, end_date):
+    seconds = methods.dt_span_seconds(start_date, end_date)
+    days = seconds / 60 / 60 / 24
+    days = round(days)
+    return days
+
+
+def add_days(dt1, days):
+    res = methods.add_interval('days', days, dt1)
+    return res
