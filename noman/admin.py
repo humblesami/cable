e29fc24e6ccc975fc667fd8e00552ac1f1c9dc89
humblesami <Prvmgo123@45>
2021-02-12 01:48:46 +0500

    applied filter on clients

--- noman/admin.py
+++ noman/admin.py
@@ -1,25 +1,37 @@
 from django.contrib import admin
 from .models import Area, Package, PackageType, Client, Payment
 
-# Register your models here.
+
 class AreaAdmin(admin.ModelAdmin):
-    list_display = ('name', 'status', 'created_at')
-    search_fields = ['name']
+    list_display = ['name', 'status', 'created_at']
     exclude = ['created_at',]
 
+
 class PackageTypeAdmin(admin.ModelAdmin):
-    list_display= ('name',)
+    list_display= ['name']
+    search_fields = ['name']
+    exclude = ['created_at']
+
 
 class PackageAdmin(admin.ModelAdmin):
-    list_display= ('name','status','package_type',)
+    list_display= ['name','status','package_type']
+    search_fields = ['name', 'package_type']
+    list_filter = ['package_type', 'status']
+    exclude = ['created_at']
+
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ('name','father_name','mobile','email','cnic','price', 'status', 'created_at')
-    search_fields = ['name']
+    list_display = ['name','mobile','email','next_payment_date','price','status']
+    search_fields = ['package']
+    exclude = ['created_at']
+
+
+class PaymentAdmin(admin.ModelAdmin):
+    exclude = ['created_at']
 
 
 admin.site.register(Area, AreaAdmin)
 admin.site.register(PackageType,PackageTypeAdmin)
 admin.site.register(Package, PackageAdmin)
-admin.site.register(Payment)
+admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Client, ClientAdmin)
--- noman/models.py
+++ noman/models.py
@@ -1,3 +1,5 @@
+import calendar
+
 from django.db import models
 from datetime import datetime
 
@@ -9,14 +11,22 @@ class Area(models.Model):
     name= models.CharField(max_length=200)
     status = models.BooleanField()
     created_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
     def save(self, *args, **kwargs):
         if not self.pk:
             self.created_at = datetime.now()
         res = super().save(args, kwargs)
         return res
 
+
 class PackageType(models.Model):
     name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Package(models.Model):
@@ -29,6 +39,8 @@ class Package(models.Model):
     package_type = models.ForeignKey(PackageType, on_delete=models.CASCADE, related_name='packages', null=True)
     created_at = models.DateTimeField()
 
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -36,28 +48,34 @@ class Package(models.Model):
         res = super().save(args, kwargs)
         return res
 
+
 class Client(models.Model):
-    class Meta:
-        ordering = ('next_payment_date', )
-        pass
-        #verbose_name_plural = 'Sami'
-        
     name= models.CharField(max_length=200)
-    mobile= models.CharField(max_length=200, null=True)
-    email= models.CharField(max_length=200, null=True)
-    cnic= models.CharField(max_length=200, null=True)
-    price= models.IntegerField()
-    connection_charges= models.IntegerField()
-    father_name= models.CharField(max_length=200, null=True)
-    connection_date =models.DateField()
-    address= models.CharField(max_length=1024, null=True)
     status = models.BooleanField()
-    pending= models.IntegerField(default=0)
+
+    price = models.IntegerField()
+    connection_charges= models.IntegerField()
+    connection_date = models.DateField()
+    pending = models.IntegerField(default=0)
     package = models.ForeignKey(Package, on_delete=models.CASCADE, related_name='clients')
-    last_payment_date = models.DateField()
+    last_payment_date = models.DateField(blank=True, null=True)
     next_payment_date = models.DateField()
+
+    father_name= models.CharField(max_length=200, null=True, blank=True)
+    mobile = models.CharField(max_length=200, null=True, blank=True)
+    email = models.CharField(max_length=200, null=True, blank=True)
+    cnic = models.CharField(max_length=200, null=True, blank=True)
+    address= models.CharField(max_length=1024, null=True, blank=True)
+
     created_at = models.DateTimeField()
 
+    class Meta:
+        ordering = ('next_payment_date',)
+        pass
+        # verbose_name_plural = 'Sami'
+
+    def __str__(self):
+        return self.name
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -78,16 +96,16 @@ def add_months(sourcedate, months):
 def add_one_month_to_date(given_date):
     return add_months(given_date, 1)
 
+
 class Payment(models.Model):
     class Meta:
         pass
         #verbose_name_plural = 'Sami'
-        
+
     client= models.ForeignKey(Client, related_name='payment', on_delete=models.CASCADE)
     amount = models.IntegerField()
     payment_date = models.DateField()
     created_at = models.DateTimeField()
-
 
     def save(self, *args, **kwargs):
         if not self.pk:
@@ -96,4 +114,3 @@ class Payment(models.Model):
             self.client.save()
         res = super().save(args, kwargs)
         return res
-    
