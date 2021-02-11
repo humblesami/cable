1a5878de5b95ab98d96a9d5ab551e82c7ca9af37
NomanHameed <nomanbutt8322@gmail.com>
2021-02-11 21:04:09 +0500

    fix done

--- noman/admin.py
+++ noman/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Area, Package, PackageType
+from .models import Area, Package, PackageType, Client, Payment
 
 # Register your models here.
 class AreaAdmin(admin.ModelAdmin):
@@ -7,7 +7,19 @@ class AreaAdmin(admin.ModelAdmin):
     search_fields = ['name']
     exclude = ['created_at',]
 
+class PackageTypeAdmin(admin.ModelAdmin):
+    list_display= ('name',)
+
+class PackageAdmin(admin.ModelAdmin):
+    list_display= ('name','status','package_type',)
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name','father_name','mobile','email','cnic','price', 'status', 'created_at')
+    search_fields = ['name']
+
 
 admin.site.register(Area, AreaAdmin)
-admin.site.register(PackageType)
-admin.site.register(Package)
+admin.site.register(PackageType,PackageTypeAdmin)
+admin.site.register(Package, PackageAdmin)
+admin.site.register(Payment)
+admin.site.register(Client, ClientAdmin)
