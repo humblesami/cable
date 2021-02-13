e2adae87672c20a7c80f75a3d97696d1fce163b7
humblesami <samiakram@live.com>
2021-02-13 11:38:08 +0500

    STATIC FILES IN DEPLOYMENT

--- cable/settings.py
+++ cable/settings.py
@@ -117,13 +117,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
 
 AUTH_PASSWORD_VALIDATORS = []
 ALLOWED_HOSTS = ['localhost', '127.0.01', 'cable-billing.herokuapp.com']
