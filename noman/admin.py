3f5723eea54c80dc521c0224619af160aba7a9bc
humblesami <Prvmgo123@45>
2021-02-14 17:00:30 +0500

    heroku

--- noman/admin.py
+++ noman/admin.py
@@ -1,43 +1,143 @@
 from django.contrib import admin
 from .models import Area, Package, PackageType, Client, ClientPackage, Subscription, Payment
+from datetime import datetime
 
 
 class AreaAdmin(admin.ModelAdmin):
     list_display = ['name', 'status', 'created_at']
-    exclude = ['created_at',]
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
 
 
 class PackageTypeAdmin(admin.ModelAdmin):
     list_display= ['name']
     search_fields = ['name']
-    exclude = ['created_at']
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
 
 
 class PackageAdmin(admin.ModelAdmin):
     list_display= ['name','package_type']
     search_fields = ['name', 'package_type']
     list_filter = ['package_type']
-    exclude = ['created_at']
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
 
 
 class ClientAdmin(admin.ModelAdmin):
-    list_display = ['area', 'name','mobile','email']
+    list_display = ['area', 'name','mobile','email', 'start_date', 'end_date', 'balance']
     search_fields = ['package', 'area']
-    exclude = ['created_at']
+    fields = ['area', 'name','mobile','email', 'cnic', 'start_date', 'end_date', 'balance']
+    readonly_fields = ['start_date', 'end_date', 'balance', 'created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
+    
+    def start_date(self, obj):
+        client_package = obj.clientpackage_set.all()
+        if client_package:
+            client_package = list(client_package)[-1]
+            subscription = client_package.subscription_set.all()
+            if subscription:
+                subscription = list(subscription)[-1]
+                return subscription.start_date
+        return ''
+    
+    def end_date(self, obj):
+        client_package = obj.clientpackage_set.all()
+        if client_package:
+            client_package = list(client_package)[-1]
+            subscription = client_package.subscription_set.all()
+            if subscription:
+                subscription = list(subscription)[-1]
+                return subscription.end_date
+        return ''
+        # last=list(obj.clientpackage_set.all()[0].subscription_set.all())[-1]
+        # return obj.client_package.subscription.start_date
+    # user_address.short_description = 'User address'
+
 
 
 class PaymentAdmin(admin.ModelAdmin):
-    exclude = ['created_at']
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
 
 
 class SubscriptionAdmin(admin.ModelAdmin):
-    exclude = ['end_date']
+    readonly_fields = ['end_date', 'created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
+
+class ClientPackageAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_at', 'updated_at', 'created_by', 'updated_by']
+
+    def save_model(self, request, obj, form, change):
+        obj.user = request.user
+        if obj.pk:
+            obj.updated_by_id = obj.user.id
+            obj.updated_at = datetime.now()
+        else:
+            obj.created_by_id = obj.user.id
+            obj.created_at = datetime.now()
+        super().save_model(request, obj, form, change)
 
 
 admin.site.register(Area, AreaAdmin)
 admin.site.register(PackageType,PackageTypeAdmin)
 admin.site.register(Package, PackageAdmin)
 admin.site.register(Client, ClientAdmin)
-admin.site.register(ClientPackage)
+admin.site.register(ClientPackage, ClientPackageAdmin)
 admin.site.register(Payment, PaymentAdmin)
 admin.site.register(Subscription, SubscriptionAdmin)
--- noman/models.py
+++ noman/models.py
@@ -1,33 +1,44 @@
 from django.db import models
 from datetime import datetime
 from .import methods
+from django.contrib.auth.models import User
 
 
-class Area(models.Model):
+class DefaultClass(models.Model):
+    created_at = models.DateTimeField(null=True, blank=True)
+    updated_at = models.DateTimeField(null=True, blank=True)
+    created_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True,
+        related_query_name='%(app_label)s_%(class)s_created_by', 
+        related_name='%(app_label)s_%(class)s_created_by'
+    )
+    updated_by = models.ForeignKey(
+        User, on_delete=models.SET_NULL, null=True, blank=True,
+        related_name='%(app_label)s_%(class)s_updated_by', 
+        related_query_name='%(app_label)s_%(class)s_updated_by'
+    )
+    class Meta:
+        abstract = True
+
+class Area(DefaultClass):
     class Meta:
         ordering  = ('name', 'created_at')
     name= models.CharField(max_length=200)
     status = models.BooleanField()
-    created_at = models.DateTimeField()
+    # created_at = models.DateTimeField()
 
     def __str__(self):
         return self.name
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.created_at = datetime.now()
-        res = super().save(args, kwargs)
-        return res
 
-
-class PackageType(models.Model):
+class PackageType(DefaultClass):
     name = models.CharField(max_length=200)
 
     def __str__(self):
         return self.name
 
 
-class Package(models.Model):
+class Package(DefaultClass):
     class Meta:
         pass
         #verbose_name_plural = 'Sami'
@@ -49,7 +60,7 @@ class Package(models.Model):
         return res
 
 
-class Client(models.Model):
+class Client(DefaultClass):
     name= models.CharField(max_length=200)
     area = models.ForeignKey(Area, related_name='clients', on_delete=models.CASCADE)
 
@@ -58,7 +69,9 @@ class Client(models.Model):
     email = models.CharField(max_length=200, null=True, blank=True)
     cnic = models.CharField(max_length=200, null=True, blank=True)
     address= models.CharField(max_length=1024, null=True, blank=True)
-
+    # balance is here...
+    
+    balance = models.IntegerField(null=True, blank=True, default=0)
     created_at = models.DateTimeField()
 
     class Meta:
@@ -72,27 +85,27 @@ class Client(models.Model):
     def is_defaulter(self):
         pass
 
-    def save(self, *args, **kwargs):
-        if not self.pk:
-            self.created_at = datetime.now()
-            self.pending = self.price + self.connection_charges
-        res = super().save(args, kwargs)
-        return res
 
-
-class ClientPackage(models.Model):
+class ClientPackage(DefaultClass):
     client = models.ForeignKey(Client, on_delete=models.CASCADE)
     package = models.ForeignKey(Package, on_delete=models.CASCADE)
-    price = models.IntegerField()
-    connection_charges = models.IntegerField()
+    price = models.IntegerField(default=0)
+    connection_charges = models.IntegerField(default=0)
     connection_date = models.DateField()
     active = models.BooleanField(default=False)
+    
 
     def __str__(self):
         return self.client.name + '-' + self.package.name
+    
+    def save(self, *args, **kwargs):
+        res = super().save(args, kwargs)
+        self.client.balance = self.price + self.connection_charges
+        self.client.save()
+        return res
+        
 
-
-class Payment(models.Model):
+class Payment(DefaultClass):
     class Meta:
         pass
         #verbose_name_plural = 'Sami'
@@ -101,6 +114,12 @@ class Payment(models.Model):
     amount = models.IntegerField()
     payment_date = models.DateField()
     created_at = models.DateTimeField()
+    is_renew = models.BooleanField(null=True, blank=True)
+
+    def __str__(self):
+        return '{}-{}-{}'.format(self.client_package.client.name, self.amount, self.payment_date)
+    
+
 
     def save(self, *args, **kwargs):
         creating = False
@@ -110,11 +129,19 @@ class Payment(models.Model):
         res = super().save(args, kwargs)
 
         if creating:
-            self.client_package.client.balance + self.amount
-            self.client_package.client.save()
+            # price = self.client_package.price if self.client_package.price else 0
+            # balance = self.client_package.client.balance if self.client_package.client.balance else 0
+            # total_payable = price + balance
+            # remaining_payables = total_payable - self.amount
+            # if remaining_payables < 0:
+            #     # in case client pay extra amount in advance
+            #     pass
+            # else:
+            #     self.client_package.client.balance = remaining_payables
+            #     self.client_package.client.save()
 
             new_subscription = Subscription(
-                client_pakage_id=self.client_package.id,
+                client_package_id=self.client_package.id,
                 start_date=self.payment_date,
                 payment_id=self.id
             )
@@ -123,11 +150,15 @@ class Payment(models.Model):
         return res
 
 
-class Subscription(models.Model):
+class Subscription(DefaultClass):
     client_package = models.ForeignKey(ClientPackage, on_delete=models.CASCADE)
     start_date = models.DateField()
     end_date = models.DateField()
     payment = models.ForeignKey(Payment, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Subscription-{}-{}-From: {}-To: {}'.format(self.client_package.client.name, self.payment.amount, self.start_date, self.end_date)
+    
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
@@ -143,9 +174,29 @@ class Subscription(models.Model):
                     diff_days = get_days_difference(last_obj.start_date, last_obj.end_date)
                     self.start_date = add_days(last_obj.start_date, diff_days)
 
-        months = price % self.client.balance
-        end_date = add_months(self.start_date, months)
-        self.end_date=end_date
+        balance = self.client_package.client.balance
+        balance = balance if balance else 0
+
+        if balance > price:
+            # in case of advance payment
+
+            # months = price % self.client_package.client.balance
+            # end_date = add_months(self.start_date, months)
+            # self.end_date=end_date
+            pass
+        else:
+            amount = self.payment.amount
+            amount = amount if amount else 0
+            payable = 0
+            if self.payment.is_renew:
+                package_price = self.client_package.price
+                package_price = package_price if package_price else 0
+                payable = balance + package_price - amount
+            else:
+                payable = balance - amount
+            self.client_package.client.balance = payable
+            self.client_package.client.save()
+            self.end_date = add_one_month_to_date(self.start_date)
         res = super().save()
         return res
 
